402dacb95b832278dbd55d3082cc375e08aab589
wormbase <hinxton@wormbase.org>
2018-05-14 11:42:31 +0100

    Use environment variable for mysql password

--- bin/iris_master.sh
+++ bin/iris_master.sh
@@ -11,16 +11,12 @@ USAGE="You need to provide the following arguments:
 -u - a file of user IDs to process (in the format avatar@apollo).
 -m - the password to the mysql server.\n"
 
-while getopts "u:m:" args
+while getopts "u:" args
 do
 	case $args in
 
 		u)
 			USERS=$OPTARG
-			;;
-
-		m)
-			MYSQL_PWORD=$OPTARG
 			;;
 
 		\?)
@@ -30,7 +26,7 @@ do
 	esac
 done
 	
-if [[ -z $USERS || -z $MYSQL_PWORD ]]
+if [[ -z $USERS ]]
 then
 	printf "$USAGE"
 	exit
@@ -39,19 +35,19 @@ fi
 DATE=`date +%Y-%m-%d`
 
 #dump and process GFFs
-$IRIS_HOME/iris/bin/process_tokens.pl --user_list $USERS --annotations_path $ANNOTATIONS_DIR_PATH --apollo_pword $APOLLO_ADMIN_PASS --mysql_pword $MYSQL_PWORD --data_path $DATA_DIR_PATH 
+$IRIS_HOME/iris/bin/process_tokens.pl --user_list $USERS --annotations_path $ANNOTATIONS_DIR_PATH --apollo_pword $APOLLO_ADMIN_PASS --mysql_pword $MYSQL_PASS --data_path $DATA_DIR_PATH 
 
 #write html files
-$IRIS_HOME/iris/bin/write_html_files.pl --annotations_path $ANNOTATIONS_DIR_PATH --mysql_pword $MYSQL_PWORD --data_path $DATA_DIR_PATH
+$IRIS_HOME/iris/bin/write_html_files.pl --annotations_path $ANNOTATIONS_DIR_PATH --mysql_pword $MYSQL_PASS --data_path $DATA_DIR_PATH
 
 #push html files to AWS
 #aws s3 cp $ANNOTATIONS_DIR_PATH/$DATE/html/ s3://iris.testbucket/ --recursive --acl public-read
 
 #back up the tokens database
-mysqldump -h mysql-wormbase-pipelines -P 4331 -u wormadmin -p$MYSQL_PWORD iris_tokens > $ANNOTATIONS_DIR_PATH/$DATE/iris_tokens.bak
+mysqldump -h mysql-wormbase-pipelines -P 4331 -u wormadmin -p$MYSQL_PASS iris_tokens > $ANNOTATIONS_DIR_PATH/$DATE/iris_tokens.bak
 
 #process annotations
-$IRIS_HOME/iris/bin/process_annotations.pl --user_list $USERS --annotations_path $ANNOTATIONS_DIR_PATH --apollo_pword $APOLLO_ADMIN_PASS --mysql_pword $MYSQL_PWORD --data_path $DATA_DIR_PATH
+$IRIS_HOME/iris/bin/process_annotations.pl --user_list $USERS --annotations_path $ANNOTATIONS_DIR_PATH --apollo_pword $APOLLO_ADMIN_PASS --mysql_pword $MYSQL_PASS --data_path $DATA_DIR_PATH
 
 #back up the annotations database
-mysqldump -h mysql-wormbase-pipelines -P 4331 -u wormadmin -p$MYSQL_PWORD iris_genes > $ANNOTATIONS_DIR_PATH/$DATE/iris_genes.bak
+mysqldump -h mysql-wormbase-pipelines -P 4331 -u wormadmin -p$MYSQL_PASS iris_genes > $ANNOTATIONS_DIR_PATH/$DATE/iris_genes.bak
